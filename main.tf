5f74bd16fb1dcb5a3c39d85c43cccd64f6c44985
Alessandro Santini <alessandro.santini@toremo.com>
2023-04-27 17:02:29 +0200

    Moved to GitHub-driven deployment

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       source = "hetznercloud/hcloud"
     }
   }
-  
+
   cloud {
     organization = "spoletum"
     workspaces {
@@ -32,13 +32,13 @@ resource "hcloud_ssh_key" "personal" {
   public_key = var.ssh_public_key
 }
 
-resource "hcloud_network" "petclinic" {
-  name     = "petclinic"
+resource "hcloud_network" "albornoz" {
+  name     = "albornoz"
   ip_range = "10.0.0.0/16"
 }
 
 resource "hcloud_network_subnet" "infra" {
-  network_id   = hcloud_network.petclinic.id
+  network_id   = hcloud_network.albornoz.id
   network_zone = "eu-central"
   type         = "cloud"
   ip_range     = "10.0.0.0/24"
@@ -46,35 +46,35 @@ resource "hcloud_network_subnet" "infra" {
 
 resource "hcloud_server_network" "name" {
   network_id = hcloud_network_subnet.infra.network_id
-  server_id  = hcloud_server.petclinic.id
+  server_id  = hcloud_server.albornoz.id
 }
 
-resource "hcloud_server" "petclinic" {
-  name        = "petclinic"
+resource "hcloud_server" "albornoz" {
+  name        = "albornoz"
   server_type = "cax31"
   image       = "fedora-37"
   datacenter  = "fsn1-dc14"
   network {
-    network_id = hcloud_network.petclinic.id
+    network_id = hcloud_network.albornoz.id
   }
   ssh_keys = [hcloud_ssh_key.personal.name]
-  # user_data = base64encode(templatefile("${path.module}/cloud-init.tpl", {
-  #   ansible_public_key = var.ansible_public_key,
-  # }))
+  user_data = base64encode(templatefile("${path.module}/templates/cloud-init.tpl", {
+    ansible_public_key = var.ansible_public_key,
+  }))
 }
 
 # resource "local_file" "inventory" {
 #   content = templatefile("${path.module}/inventory.tpl",
 #     {
-#       dev01         = hcloud_server.petclinic.ipv4_address
-#       dev01_private = tolist(hcloud_server.petclinic.network)[0].ip
+#       dev01         = hcloud_server.albornoz.ipv4_address
+#       dev01_private = tolist(hcloud_server.albornoz.network)[0].ip
 #     }
 #   )
 #   filename = "../ansible/inventory.ini"
 # }
 
-resource "hcloud_rdns" "petclinic" {
+resource "hcloud_rdns" "albornoz" {
   dns_ptr    = "spoletum.net"
-  ip_address = hcloud_server.petclinic.ipv4_address
-  server_id  = hcloud_server.petclinic.id
+  ip_address = hcloud_server.albornoz.ipv4_address
+  server_id  = hcloud_server.albornoz.id
 }
---
+++ templates/cloud-init.tpl
@@ -0,0 +1,13 @@
+#cloud-config
+bootcmd:
+  - echo 'PermitRootLogin no' | sudo tee -a /etc/ssh/sshd_config
+runcmd:
+  - systemctl restart sshd
+users:
+  - name: ansible
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: wheel
+    ssh_import_id: None
+    lock_passwd: true
+    ssh_authorized_keys:
+      - ${ansible_public_key}
