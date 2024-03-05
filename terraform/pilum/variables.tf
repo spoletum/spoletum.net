eb2a88b3825c6abe60ddbf0c5da6ec6cec968c71
Alessandro Santini <alessandro.santini@toremo.com>
2024-03-06 00:10:06 +0100

    Updated base image with microk8s

--- terraform/pilum/variables.tf
+++ terraform/pilum/variables.tf
@@ -7,5 +7,5 @@ variable "ssh_public_key" {
 variable "image_id" {
   description = "The ID of the image to use for the server"
   type        = string
-  default     = "152989673"
+  default     = "ubuntu-22.04"
 }
