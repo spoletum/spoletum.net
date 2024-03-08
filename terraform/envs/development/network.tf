05a9e400196ad5559136b85f695a08aa56edfe10
Alessandro Santini <alessandro.santini@toremo.com>
2024-03-08 16:58:51 +0100

    Refactoring networking

--- terraform/envs/development/network.tf
+++ terraform/envs/development/network.tf
@@ -1,12 +1,12 @@
 resource "hcloud_network" "development" {
   name     = "external"
-  ip_range = "10.0.0.0/24"
+  ip_range = "10.0.0.0/16"
 }
 
 resource "hcloud_network_subnet" "internal" {
   for_each     = toset(var.subnets)
   ip_range     = each.value.ip_range
-  network_zone = each.network_zone
+  network_zone = each.zone
   network_id   = hcloud_network.development.id
   type         = each.type
 }
